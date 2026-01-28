e83e167b3df9761c8aeaba2558da2a8073de5c16
1999AZZAR <azzar.mr.zs@gmail.com>
2026-01-29 02:49:06 +0700

    Update Flask host to 0.0.0.0 for network-wide accessibility

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ if __name__ == '__main__':
         # (Naive check, relying on init_db IF NOT EXISTS)
         database.init_db()
 
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
